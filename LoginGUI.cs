ed060da6c4fda4980161a65205572074f7c4b6b2
Bilal CIFCI <cifci.bilal@outlook.com>
2024-03-17 02:15:28 +0300

    LoginGUI resimleri tasarlandi

--- LoginGUI.cs
+++ LoginGUI.cs
@@ -1,8 +1,8 @@
 namespace HospitalManagementSystem
 {
-    public partial class Form1 : Form
+    public partial class LoginGUI : Form
     {
-        public Form1()
+        public LoginGUI()
         {
             InitializeComponent();
         }
