a2fb833288098c1f901327da05716636a9d3f6d8
Bilal CIFCI <cifci.bilal@outlook.com>
2024-03-17 09:42:23 +0300

    Sekreter login ekrani tasarlandi -SecretaryLoginGUI

--- Forms/UserControllers/PatientLoginGUI.Designer.cs
+++ Forms/UserControllers/PatientLoginGUI.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             lbl_PatientLoginGUI = new Label();
-            label1 = new Label();
-            rjTextBox1 = new CustomTools.RJTextBox.RJTextBox();
-            rjTextBox2 = new CustomTools.RJTextBox.RJTextBox();
-            label2 = new Label();
-            rjButton1 = new CustomTools.RJButton.RJButton();
-            rjButton2 = new CustomTools.RJButton.RJButton();
-            rjBtnBackMainForm = new CustomTools.RJButton.RJButton();
+            label_Patient_ID = new Label();
+            rjTextBoxPatientIdentificationNo = new CustomTools.RJTextBox.RJTextBox();
+            rjTextBoxPatientPassword = new CustomTools.RJTextBox.RJTextBox();
+            label_Patient_Password = new Label();
+            rjBtnPatientSignIn = new CustomTools.RJButton.RJButton();
+            rjBtnPatientSignUp = new CustomTools.RJButton.RJButton();
+            rjBtnPatientBackMain = new CustomTools.RJButton.RJButton();
             SuspendLayout();
             // 
             // lbl_PatientLoginGUI
@@ -48,138 +48,138 @@
             lbl_PatientLoginGUI.TabIndex = 0;
             lbl_PatientLoginGUI.Text = "Ready to Login";
             // 
-            // label1
+            // label_Patient_ID
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Yu Gothic UI Semibold", 11F);
-            label1.Location = new Point(75, 111);
-            label1.Name = "label1";
-            label1.Size = new Size(124, 20);
-            label1.TabIndex = 1;
-            label1.Text = "Identification No";
+            label_Patient_ID.AutoSize = true;
+            label_Patient_ID.Font = new Font("Yu Gothic UI Semibold", 11F);
+            label_Patient_ID.Location = new Point(75, 111);
+            label_Patient_ID.Name = "label_Patient_ID";
+            label_Patient_ID.Size = new Size(124, 20);
+            label_Patient_ID.TabIndex = 1;
+            label_Patient_ID.Text = "Identification No";
             // 
-            // rjTextBox1
+            // rjTextBoxPatientIdentificationNo
             // 
-            rjTextBox1.BackColor = SystemColors.Window;
-            rjTextBox1.BorderColor = Color.Salmon;
-            rjTextBox1.BorderFocusColor = Color.HotPink;
-            rjTextBox1.BorderRadius = 15;
-            rjTextBox1.BorderSize = 2;
-            rjTextBox1.Font = new Font("Microsoft Sans Serif", 9.5F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            rjTextBox1.ForeColor = Color.FromArgb(64, 64, 64);
-            rjTextBox1.Location = new Point(75, 135);
-            rjTextBox1.Margin = new Padding(4);
-            rjTextBox1.Multiline = false;
-            rjTextBox1.Name = "rjTextBox1";
-            rjTextBox1.Padding = new Padding(10, 7, 10, 7);
-            rjTextBox1.PasswordChar = false;
-            rjTextBox1.PlaceholderColor = Color.DarkGray;
-            rjTextBox1.PlaceholderText = "";
-            rjTextBox1.Size = new Size(250, 31);
-            rjTextBox1.TabIndex = 2;
-            rjTextBox1.Texts = "";
-            rjTextBox1.UnderlinedStyle = false;
+            rjTextBoxPatientIdentificationNo.BackColor = SystemColors.Window;
+            rjTextBoxPatientIdentificationNo.BorderColor = Color.Salmon;
+            rjTextBoxPatientIdentificationNo.BorderFocusColor = Color.HotPink;
+            rjTextBoxPatientIdentificationNo.BorderRadius = 15;
+            rjTextBoxPatientIdentificationNo.BorderSize = 2;
+            rjTextBoxPatientIdentificationNo.Font = new Font("Microsoft Sans Serif", 9.5F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            rjTextBoxPatientIdentificationNo.ForeColor = Color.FromArgb(64, 64, 64);
+            rjTextBoxPatientIdentificationNo.Location = new Point(75, 135);
+            rjTextBoxPatientIdentificationNo.Margin = new Padding(4);
+            rjTextBoxPatientIdentificationNo.Multiline = false;
+            rjTextBoxPatientIdentificationNo.Name = "rjTextBoxPatientIdentificationNo";
+            rjTextBoxPatientIdentificationNo.Padding = new Padding(10, 7, 10, 7);
+            rjTextBoxPatientIdentificationNo.PasswordChar = false;
+            rjTextBoxPatientIdentificationNo.PlaceholderColor = Color.DarkGray;
+            rjTextBoxPatientIdentificationNo.PlaceholderText = "";
+            rjTextBoxPatientIdentificationNo.Size = new Size(250, 31);
+            rjTextBoxPatientIdentificationNo.TabIndex = 2;
+            rjTextBoxPatientIdentificationNo.Texts = "";
+            rjTextBoxPatientIdentificationNo.UnderlinedStyle = false;
             // 
-            // rjTextBox2
+            // rjTextBoxPatientPassword
             // 
-            rjTextBox2.BackColor = SystemColors.Window;
-            rjTextBox2.BorderColor = Color.Salmon;
-            rjTextBox2.BorderFocusColor = Color.HotPink;
-            rjTextBox2.BorderRadius = 15;
-            rjTextBox2.BorderSize = 2;
-            rjTextBox2.Font = new Font("Microsoft Sans Serif", 9.5F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            rjTextBox2.ForeColor = Color.FromArgb(64, 64, 64);
-            rjTextBox2.Location = new Point(75, 201);
-            rjTextBox2.Margin = new Padding(4);
-            rjTextBox2.Multiline = false;
-            rjTextBox2.Name = "rjTextBox2";
-            rjTextBox2.Padding = new Padding(10, 7, 10, 7);
-            rjTextBox2.PasswordChar = false;
-            rjTextBox2.PlaceholderColor = Color.DarkGray;
-            rjTextBox2.PlaceholderText = "";
-            rjTextBox2.Size = new Size(250, 31);
-            rjTextBox2.TabIndex = 4;
-            rjTextBox2.Texts = "";
-            rjTextBox2.UnderlinedStyle = false;
+            rjTextBoxPatientPassword.BackColor = SystemColors.Window;
+            rjTextBoxPatientPassword.BorderColor = Color.Salmon;
+            rjTextBoxPatientPassword.BorderFocusColor = Color.HotPink;
+            rjTextBoxPatientPassword.BorderRadius = 15;
+            rjTextBoxPatientPassword.BorderSize = 2;
+            rjTextBoxPatientPassword.Font = new Font("Microsoft Sans Serif", 9.5F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            rjTextBoxPatientPassword.ForeColor = Color.FromArgb(64, 64, 64);
+            rjTextBoxPatientPassword.Location = new Point(75, 201);
+            rjTextBoxPatientPassword.Margin = new Padding(4);
+            rjTextBoxPatientPassword.Multiline = false;
+            rjTextBoxPatientPassword.Name = "rjTextBoxPatientPassword";
+            rjTextBoxPatientPassword.Padding = new Padding(10, 7, 10, 7);
+            rjTextBoxPatientPassword.PasswordChar = false;
+            rjTextBoxPatientPassword.PlaceholderColor = Color.DarkGray;
+            rjTextBoxPatientPassword.PlaceholderText = "";
+            rjTextBoxPatientPassword.Size = new Size(250, 31);
+            rjTextBoxPatientPassword.TabIndex = 4;
+            rjTextBoxPatientPassword.Texts = "";
+            rjTextBoxPatientPassword.UnderlinedStyle = false;
             // 
-            // label2
+            // label_Patient_Password
             // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Yu Gothic UI Semibold", 11F);
-            label2.Location = new Point(75, 179);
-            label2.Name = "label2";
-            label2.Size = new Size(73, 20);
-            label2.TabIndex = 3;
-            label2.Text = "Password";
+            label_Patient_Password.AutoSize = true;
+            label_Patient_Password.Font = new Font("Yu Gothic UI Semibold", 11F);
+            label_Patient_Password.Location = new Point(75, 179);
+            label_Patient_Password.Name = "label_Patient_Password";
+            label_Patient_Password.Size = new Size(73, 20);
+            label_Patient_Password.TabIndex = 3;
+            label_Patient_Password.Text = "Password";
             // 
-            // rjButton1
+            // rjBtnPatientSignIn
             // 
-            rjButton1.BackColor = Color.Black;
-            rjButton1.BackgroundColor = Color.Black;
-            rjButton1.BorderColor = Color.Salmon;
-            rjButton1.BorderRadius = 17;
-            rjButton1.BorderSize = 0;
-            rjButton1.FlatAppearance.BorderSize = 0;
-            rjButton1.FlatStyle = FlatStyle.Flat;
-            rjButton1.Font = new Font("Yu Gothic UI Semibold", 10F);
-            rjButton1.ForeColor = Color.White;
-            rjButton1.Location = new Point(130, 253);
-            rjButton1.Name = "rjButton1";
-            rjButton1.Size = new Size(140, 31);
-            rjButton1.TabIndex = 5;
-            rjButton1.Text = "Sign in";
-            rjButton1.TextColor = Color.White;
-            rjButton1.UseVisualStyleBackColor = false;
+            rjBtnPatientSignIn.BackColor = Color.Black;
+            rjBtnPatientSignIn.BackgroundColor = Color.Black;
+            rjBtnPatientSignIn.BorderColor = Color.Salmon;
+            rjBtnPatientSignIn.BorderRadius = 17;
+            rjBtnPatientSignIn.BorderSize = 0;
+            rjBtnPatientSignIn.FlatAppearance.BorderSize = 0;
+            rjBtnPatientSignIn.FlatStyle = FlatStyle.Flat;
+            rjBtnPatientSignIn.Font = new Font("Yu Gothic UI Semibold", 10F);
+            rjBtnPatientSignIn.ForeColor = Color.White;
+            rjBtnPatientSignIn.Location = new Point(130, 253);
+            rjBtnPatientSignIn.Name = "rjBtnPatientSignIn";
+            rjBtnPatientSignIn.Size = new Size(140, 31);
+            rjBtnPatientSignIn.TabIndex = 5;
+            rjBtnPatientSignIn.Text = "Sign in";
+            rjBtnPatientSignIn.TextColor = Color.White;
+            rjBtnPatientSignIn.UseVisualStyleBackColor = false;
             // 
-            // rjButton2
+            // rjBtnPatientSignUp
             // 
-            rjButton2.BackColor = Color.Black;
-            rjButton2.BackgroundColor = Color.Black;
-            rjButton2.BorderColor = Color.Salmon;
-            rjButton2.BorderRadius = 17;
-            rjButton2.BorderSize = 0;
-            rjButton2.FlatAppearance.BorderSize = 0;
-            rjButton2.FlatStyle = FlatStyle.Flat;
-            rjButton2.Font = new Font("Yu Gothic UI Semibold", 10F);
-            rjButton2.ForeColor = Color.White;
-            rjButton2.Location = new Point(323, 7);
-            rjButton2.Name = "rjButton2";
-            rjButton2.Size = new Size(74, 31);
-            rjButton2.TabIndex = 6;
-            rjButton2.Text = "Sign Up";
-            rjButton2.TextColor = Color.White;
-            rjButton2.UseVisualStyleBackColor = false;
+            rjBtnPatientSignUp.BackColor = Color.Black;
+            rjBtnPatientSignUp.BackgroundColor = Color.Black;
+            rjBtnPatientSignUp.BorderColor = Color.Salmon;
+            rjBtnPatientSignUp.BorderRadius = 17;
+            rjBtnPatientSignUp.BorderSize = 0;
+            rjBtnPatientSignUp.FlatAppearance.BorderSize = 0;
+            rjBtnPatientSignUp.FlatStyle = FlatStyle.Flat;
+            rjBtnPatientSignUp.Font = new Font("Yu Gothic UI Semibold", 10F);
+            rjBtnPatientSignUp.ForeColor = Color.White;
+            rjBtnPatientSignUp.Location = new Point(323, 7);
+            rjBtnPatientSignUp.Name = "rjBtnPatientSignUp";
+            rjBtnPatientSignUp.Size = new Size(74, 31);
+            rjBtnPatientSignUp.TabIndex = 6;
+            rjBtnPatientSignUp.Text = "Sign Up";
+            rjBtnPatientSignUp.TextColor = Color.White;
+            rjBtnPatientSignUp.UseVisualStyleBackColor = false;
             // 
-            // rjBtnBackMainForm
+            // rjBtnPatientBackMain
             // 
-            rjBtnBackMainForm.BackColor = Color.White;
-            rjBtnBackMainForm.BackgroundColor = Color.White;
-            rjBtnBackMainForm.BorderColor = Color.RosyBrown;
-            rjBtnBackMainForm.BorderRadius = 20;
-            rjBtnBackMainForm.BorderSize = 0;
-            rjBtnBackMainForm.FlatAppearance.BorderSize = 0;
-            rjBtnBackMainForm.FlatStyle = FlatStyle.Flat;
-            rjBtnBackMainForm.ForeColor = Color.White;
-            rjBtnBackMainForm.Image = Properties.Resources.LeftArrowIcon;
-            rjBtnBackMainForm.Location = new Point(3, 3);
-            rjBtnBackMainForm.Name = "rjBtnBackMainForm";
-            rjBtnBackMainForm.Size = new Size(40, 40);
-            rjBtnBackMainForm.TabIndex = 7;
-            rjBtnBackMainForm.TextColor = Color.White;
-            rjBtnBackMainForm.UseVisualStyleBackColor = false;
+            rjBtnPatientBackMain.BackColor = Color.White;
+            rjBtnPatientBackMain.BackgroundColor = Color.White;
+            rjBtnPatientBackMain.BorderColor = Color.RosyBrown;
+            rjBtnPatientBackMain.BorderRadius = 20;
+            rjBtnPatientBackMain.BorderSize = 0;
+            rjBtnPatientBackMain.FlatAppearance.BorderSize = 0;
+            rjBtnPatientBackMain.FlatStyle = FlatStyle.Flat;
+            rjBtnPatientBackMain.ForeColor = Color.White;
+            rjBtnPatientBackMain.Image = Properties.Resources.LeftArrowIcon;
+            rjBtnPatientBackMain.Location = new Point(3, 3);
+            rjBtnPatientBackMain.Name = "rjBtnPatientBackMain";
+            rjBtnPatientBackMain.Size = new Size(40, 40);
+            rjBtnPatientBackMain.TabIndex = 7;
+            rjBtnPatientBackMain.TextColor = Color.White;
+            rjBtnPatientBackMain.UseVisualStyleBackColor = false;
             // 
             // PatientLoginGUI
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            Controls.Add(rjBtnBackMainForm);
-            Controls.Add(rjButton2);
-            Controls.Add(rjButton1);
-            Controls.Add(rjTextBox2);
-            Controls.Add(label2);
-            Controls.Add(rjTextBox1);
-            Controls.Add(label1);
+            Controls.Add(rjBtnPatientBackMain);
+            Controls.Add(rjBtnPatientSignUp);
+            Controls.Add(rjBtnPatientSignIn);
+            Controls.Add(rjTextBoxPatientPassword);
+            Controls.Add(label_Patient_Password);
+            Controls.Add(rjTextBoxPatientIdentificationNo);
+            Controls.Add(label_Patient_ID);
             Controls.Add(lbl_PatientLoginGUI);
             Name = "PatientLoginGUI";
             Size = new Size(400, 350);
@@ -190,12 +190,12 @@
         #endregion
 
         private Label lbl_PatientLoginGUI;
-        private Label label1;
-        private CustomTools.RJTextBox.RJTextBox rjTextBox1;
-        private CustomTools.RJTextBox.RJTextBox rjTextBox2;
-        private Label label2;
-        private CustomTools.RJButton.RJButton rjButton1;
-        private CustomTools.RJButton.RJButton rjButton2;
-        private CustomTools.RJButton.RJButton rjBtnBackMainForm;
+        private Label label_Patient_ID;
+        private CustomTools.RJTextBox.RJTextBox rjTextBoxPatientIdentificationNo;
+        private CustomTools.RJTextBox.RJTextBox rjTextBoxPatientPassword;
+        private Label label_Patient_Password;
+        private CustomTools.RJButton.RJButton rjBtnPatientSignIn;
+        private CustomTools.RJButton.RJButton rjBtnPatientSignUp;
+        private CustomTools.RJButton.RJButton rjBtnPatientBackMain;
     }
 }
---
+++ Forms/UserControllers/SecretaryLoginGUI.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace HospitalManagementSystem.Forms.UserControllers
+{
+    partial class SecretaryLoginGUI
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            rjBtnSecretaryBackMain = new CustomTools.RJButton.RJButton();
+            rjBtnSecretarySignIn = new CustomTools.RJButton.RJButton();
+            rjTextBoxSecretaryPassword = new CustomTools.RJTextBox.RJTextBox();
+            label_Secretary_Password = new Label();
+            rjTextBoxSecretaryIdentificationNo = new CustomTools.RJTextBox.RJTextBox();
+            label_Secretary_ID = new Label();
+            lbl_SecretaryLoginGUI = new Label();
+            SuspendLayout();
+            // 
+            // rjBtnSecretaryBackMain
+            // 
+            rjBtnSecretaryBackMain.BackColor = Color.White;
+            rjBtnSecretaryBackMain.BackgroundColor = Color.White;
+            rjBtnSecretaryBackMain.BorderColor = Color.RosyBrown;
+            rjBtnSecretaryBackMain.BorderRadius = 20;
+            rjBtnSecretaryBackMain.BorderSize = 0;
+            rjBtnSecretaryBackMain.FlatAppearance.BorderSize = 0;
+            rjBtnSecretaryBackMain.FlatStyle = FlatStyle.Flat;
+            rjBtnSecretaryBackMain.ForeColor = Color.White;
+            rjBtnSecretaryBackMain.Image = Properties.Resources.LeftArrowIcon;
+            rjBtnSecretaryBackMain.Location = new Point(3, 3);
+            rjBtnSecretaryBackMain.Name = "rjBtnSecretaryBackMain";
+            rjBtnSecretaryBackMain.Size = new Size(40, 40);
+            rjBtnSecretaryBackMain.TabIndex = 14;
+            rjBtnSecretaryBackMain.TextColor = Color.White;
+            rjBtnSecretaryBackMain.UseVisualStyleBackColor = false;
+            // 
+            // rjBtnSecretarySignIn
+            // 
+            rjBtnSecretarySignIn.BackColor = Color.Black;
+            rjBtnSecretarySignIn.BackgroundColor = Color.Black;
+            rjBtnSecretarySignIn.BorderColor = Color.Salmon;
+            rjBtnSecretarySignIn.BorderRadius = 17;
+            rjBtnSecretarySignIn.BorderSize = 0;
+            rjBtnSecretarySignIn.FlatAppearance.BorderSize = 0;
+            rjBtnSecretarySignIn.FlatStyle = FlatStyle.Flat;
+            rjBtnSecretarySignIn.Font = new Font("Yu Gothic UI Semibold", 10F);
+            rjBtnSecretarySignIn.ForeColor = Color.White;
+            rjBtnSecretarySignIn.Location = new Point(130, 253);
+            rjBtnSecretarySignIn.Name = "rjBtnSecretarySignIn";
+            rjBtnSecretarySignIn.Size = new Size(140, 31);
+            rjBtnSecretarySignIn.TabIndex = 13;
+            rjBtnSecretarySignIn.Text = "Sign in";
+            rjBtnSecretarySignIn.TextColor = Color.White;
+            rjBtnSecretarySignIn.UseVisualStyleBackColor = false;
+            // 
+            // rjTextBoxSecretaryPassword
+            // 
+            rjTextBoxSecretaryPassword.BackColor = SystemColors.Window;
+            rjTextBoxSecretaryPassword.BorderColor = Color.Salmon;
+            rjTextBoxSecretaryPassword.BorderFocusColor = Color.HotPink;
+            rjTextBoxSecretaryPassword.BorderRadius = 15;
+            rjTextBoxSecretaryPassword.BorderSize = 2;
+            rjTextBoxSecretaryPassword.Font = new Font("Microsoft Sans Serif", 9.5F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            rjTextBoxSecretaryPassword.ForeColor = Color.FromArgb(64, 64, 64);
+            rjTextBoxSecretaryPassword.Location = new Point(75, 201);
+            rjTextBoxSecretaryPassword.Margin = new Padding(4);
+            rjTextBoxSecretaryPassword.Multiline = false;
+            rjTextBoxSecretaryPassword.Name = "rjTextBoxSecretaryPassword";
+            rjTextBoxSecretaryPassword.Padding = new Padding(10, 7, 10, 7);
+            rjTextBoxSecretaryPassword.PasswordChar = false;
+            rjTextBoxSecretaryPassword.PlaceholderColor = Color.DarkGray;
+            rjTextBoxSecretaryPassword.PlaceholderText = "";
+            rjTextBoxSecretaryPassword.Size = new Size(250, 31);
+            rjTextBoxSecretaryPassword.TabIndex = 12;
+            rjTextBoxSecretaryPassword.Texts = "";
+            rjTextBoxSecretaryPassword.UnderlinedStyle = false;
+            // 
+            // label_Secretary_Password
+            // 
+            label_Secretary_Password.AutoSize = true;
+            label_Secretary_Password.Font = new Font("Yu Gothic UI Semibold", 11F);
+            label_Secretary_Password.Location = new Point(75, 179);
+            label_Secretary_Password.Name = "label_Secretary_Password";
+            label_Secretary_Password.Size = new Size(73, 20);
+            label_Secretary_Password.TabIndex = 11;
+            label_Secretary_Password.Text = "Password";
+            // 
+            // rjTextBoxSecretaryIdentificationNo
+            // 
+            rjTextBoxSecretaryIdentificationNo.BackColor = SystemColors.Window;
+            rjTextBoxSecretaryIdentificationNo.BorderColor = Color.Salmon;
+            rjTextBoxSecretaryIdentificationNo.BorderFocusColor = Color.HotPink;
+            rjTextBoxSecretaryIdentificationNo.BorderRadius = 15;
+            rjTextBoxSecretaryIdentificationNo.BorderSize = 2;
+            rjTextBoxSecretaryIdentificationNo.Font = new Font("Microsoft Sans Serif", 9.5F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            rjTextBoxSecretaryIdentificationNo.ForeColor = Color.FromArgb(64, 64, 64);
+            rjTextBoxSecretaryIdentificationNo.Location = new Point(75, 135);
+            rjTextBoxSecretaryIdentificationNo.Margin = new Padding(4);
+            rjTextBoxSecretaryIdentificationNo.Multiline = false;
+            rjTextBoxSecretaryIdentificationNo.Name = "rjTextBoxSecretaryIdentificationNo";
+            rjTextBoxSecretaryIdentificationNo.Padding = new Padding(10, 7, 10, 7);
+            rjTextBoxSecretaryIdentificationNo.PasswordChar = false;
+            rjTextBoxSecretaryIdentificationNo.PlaceholderColor = Color.DarkGray;
+            rjTextBoxSecretaryIdentificationNo.PlaceholderText = "";
+            rjTextBoxSecretaryIdentificationNo.Size = new Size(250, 31);
+            rjTextBoxSecretaryIdentificationNo.TabIndex = 10;
+            rjTextBoxSecretaryIdentificationNo.Texts = "";
+            rjTextBoxSecretaryIdentificationNo.UnderlinedStyle = false;
+            // 
+            // label_Secretary_ID
+            // 
+            label_Secretary_ID.AutoSize = true;
+            label_Secretary_ID.Font = new Font("Yu Gothic UI Semibold", 11F);
+            label_Secretary_ID.Location = new Point(75, 111);
+            label_Secretary_ID.Name = "label_Secretary_ID";
+            label_Secretary_ID.Size = new Size(124, 20);
+            label_Secretary_ID.TabIndex = 9;
+            label_Secretary_ID.Text = "Identification No";
+            // 
+            // lbl_SecretaryLoginGUI
+            // 
+            lbl_SecretaryLoginGUI.AutoSize = true;
+            lbl_SecretaryLoginGUI.Font = new Font("Yu Gothic UI Semibold", 13F);
+            lbl_SecretaryLoginGUI.Location = new Point(132, 62);
+            lbl_SecretaryLoginGUI.Name = "lbl_SecretaryLoginGUI";
+            lbl_SecretaryLoginGUI.Size = new Size(137, 25);
+            lbl_SecretaryLoginGUI.TabIndex = 8;
+            lbl_SecretaryLoginGUI.Text = "Ready to Login";
+            // 
+            // SecretaryLoginGUI
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            Controls.Add(rjBtnSecretaryBackMain);
+            Controls.Add(rjBtnSecretarySignIn);
+            Controls.Add(rjTextBoxSecretaryPassword);
+            Controls.Add(label_Secretary_Password);
+            Controls.Add(rjTextBoxSecretaryIdentificationNo);
+            Controls.Add(label_Secretary_ID);
+            Controls.Add(lbl_SecretaryLoginGUI);
+            Name = "SecretaryLoginGUI";
+            Size = new Size(400, 350);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private CustomTools.RJButton.RJButton rjBtnSecretaryBackMain;
+        private CustomTools.RJButton.RJButton rjBtnSecretarySignIn;
+        private CustomTools.RJTextBox.RJTextBox rjTextBoxSecretaryPassword;
+        private Label label_Secretary_Password;
+        private CustomTools.RJTextBox.RJTextBox rjTextBoxSecretaryIdentificationNo;
+        private Label label_Secretary_ID;
+        private Label lbl_SecretaryLoginGUI;
+    }
+}
