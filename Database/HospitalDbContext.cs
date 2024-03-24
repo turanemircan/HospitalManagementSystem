347baa1abfbac8ccad8091f954b952fb7d69b86d
Bilal CIFCI <cifci.bilal@outlook.com>
2024-03-24 22:12:46 +0300

    sunum1

--- Database/HospitalDbContext.cs
+++ Database/HospitalDbContext.cs
@@ -10,9 +10,63 @@ namespace HospitalManagementSystem.Database
     public class HospitalDbContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Secretary> Secretarys { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(u => u.Id).ValueGeneratedOnAdd(); // Set Id as auto-incrementing
+                entity.HasKey(u => u.Id);
+                entity.Property(u => u.name).IsRequired().HasMaxLength(50);
+                entity.Property(u => u.surname).IsRequired().HasMaxLength(50);
+                entity.Property(u => u.identification).IsRequired().HasMaxLength(50);
+                entity.Property(u => u.GSM_No).IsRequired().HasMaxLength(50);
+                entity.Property(u => u.password).IsRequired().HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Doctor>(entity =>
+            {
+                entity.HasOne(d => d.user)
+                    .WithOne()
+                    .HasForeignKey<Doctor>(d => d.Id)
+                    .OnDelete(DeleteBehavior.Restrict);
+                entity.Property(d => d.branch).IsRequired().HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Patient>(entity =>
+            {
+                entity.HasOne(p => p.user)
+                    .WithOne()
+                    .HasForeignKey<Patient>(p => p.Id)
+                    .OnDelete(DeleteBehavior.Restrict);
+                entity.Property(p => p.againPassword).IsRequired().HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Secretary>(entity =>
+            {
+                entity.HasOne(s => s.user)
+                    .WithOne()
+                    .HasForeignKey<Secretary>(s => s.Id)
+                    .OnDelete(DeleteBehavior.Restrict);
+                entity.Property(s => s.locationClinic).IsRequired().HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Admin>(entity =>
+            {
+                entity.HasOne(a => a.user)
+                    .WithOne()
+                    .HasForeignKey<Admin>(a => a.Id)
+                    .OnDelete(DeleteBehavior.Restrict);
+                entity.Property(a => a.startDateOfEmployment).IsRequired();
+            });
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS;Initial Catalog=HospitalManagementSystemDatabase;Integrated Security=True;Trust Server Certificate=True");
+            optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS;Initial Catalog=HospitalManagementSystem;Integrated Security=True;Trust Server Certificate=True");
+
         }
     }
 }
--- LoginGUI.cs
+++ LoginGUI.cs
@@ -19,6 +19,52 @@ namespace HospitalManagementSystem
             helper = new Helper.Helper(panelFormController);
             MainLoginButtons mainLoginButtons = new MainLoginButtons(panelFormController);
             helper.formGoster(mainLoginButtons, mainLoginButtons.Name);
+
+            var hasta = new Patient()
+            {
+                name = "bilal",
+                surname = "cifci",
+                GSM_No = "05441220426",
+                identification = "34786885728",
+                password = "Password",
+                againPassword = "Password"
+            };
+            var doktor = new Doctor()
+            {
+                name = "bilal",
+                surname = "cifci",
+                GSM_No = "05441220426",
+                identification = "34786885728",
+                password = "Password",
+                branch = "Kalp"
+            };
+            var sekreter = new Secretary()
+            {
+                name = "bilal",
+                surname = "cifci",
+                GSM_No = "05441220426",
+                identification = "34786885728",
+                password = "Password",
+                locationClinic = "Yesil Alan"
+            };
+            var admin = new Admin()
+            {
+                name = "bilal",
+                surname = "cifci",
+                GSM_No = "05441220426",
+                identification = "34786885728",
+                password = "Password",
+                
+            };
+
+            using (var context = new HospitalDbContext())
+            {
+                context.Patients.Add(hasta);
+                context.Doctors.Add(doktor);
+                context.Admins.Add(admin);
+                context.Secretarys.Add(sekreter);
+                //context.SaveChanges();
+            }
         }
     }
 }
