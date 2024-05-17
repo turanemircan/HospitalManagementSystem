00231ee3ee263c337ced3941b1ff8f1211b5fd50
Emircan <115572710+turanemircan@users.noreply.github.com>
2024-05-17 15:22:40 +0300

    Doctor Ekranı Güncellendi

---
+++ Forms/AfterLoginForms/DALF_RandevuGörüntüle.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace HospitalManagementSystem.Forms.AfterLoginForms
+{
+    partial class DALF_RandevuGörüntüle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            buttonDALF_Upd = new CustomTools.RJButton.RJButton();
+            buttonDALF_Del = new CustomTools.RJButton.RJButton();
+            buttonDALF_Save = new CustomTools.RJButton.RJButton();
+            rjTextBox2 = new CustomTools.RJTextBox.RJTextBox();
+            rjTextBoxAdminPassword = new CustomTools.RJTextBox.RJTextBox();
+            rjTextBox1 = new CustomTools.RJTextBox.RJTextBox();
+            rjTextBoxAdminIdentificationNo = new CustomTools.RJTextBox.RJTextBox();
+            labelDALF_Presc = new Label();
+            labelDALF_Diag = new Label();
+            labelDALF_PatiId = new Label();
+            labelDALF_Patinam = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(338, 21);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(450, 417);
+            dataGridView1.TabIndex = 2;
+            // 
+            // buttonDALF_Upd
+            // 
+            buttonDALF_Upd.BackColor = Color.SkyBlue;
+            buttonDALF_Upd.BackgroundColor = Color.SkyBlue;
+            buttonDALF_Upd.BorderColor = Color.Salmon;
+            buttonDALF_Upd.BorderRadius = 10;
+            buttonDALF_Upd.BorderSize = 0;
+            buttonDALF_Upd.FlatAppearance.BorderSize = 0;
+            buttonDALF_Upd.FlatStyle = FlatStyle.Flat;
+            buttonDALF_Upd.Font = new Font("Yu Gothic UI Semibold", 10F);
+            buttonDALF_Upd.ForeColor = Color.White;
+            buttonDALF_Upd.Location = new Point(152, 318);
+            buttonDALF_Upd.Margin = new Padding(3, 4, 3, 4);
+            buttonDALF_Upd.Name = "buttonDALF_Upd";
+            buttonDALF_Upd.Size = new Size(178, 36);
+            buttonDALF_Upd.TabIndex = 34;
+            buttonDALF_Upd.Text = "Update";
+            buttonDALF_Upd.TextColor = Color.White;
+            buttonDALF_Upd.UseVisualStyleBackColor = false;
+            // 
+            // buttonDALF_Del
+            // 
+            buttonDALF_Del.BackColor = Color.Red;
+            buttonDALF_Del.BackgroundColor = Color.Red;
+            buttonDALF_Del.BorderColor = Color.Salmon;
+            buttonDALF_Del.BorderRadius = 10;
+            buttonDALF_Del.BorderSize = 0;
+            buttonDALF_Del.FlatAppearance.BorderSize = 0;
+            buttonDALF_Del.FlatStyle = FlatStyle.Flat;
+            buttonDALF_Del.Font = new Font("Yu Gothic UI Semibold", 10F);
+            buttonDALF_Del.ForeColor = Color.White;
+            buttonDALF_Del.Location = new Point(12, 318);
+            buttonDALF_Del.Margin = new Padding(3, 4, 3, 4);
+            buttonDALF_Del.Name = "buttonDALF_Del";
+            buttonDALF_Del.Size = new Size(134, 36);
+            buttonDALF_Del.TabIndex = 35;
+            buttonDALF_Del.Text = "Delete";
+            buttonDALF_Del.TextColor = Color.White;
+            buttonDALF_Del.UseVisualStyleBackColor = false;
+            // 
+            // buttonDALF_Save
+            // 
+            buttonDALF_Save.BackColor = Color.Salmon;
+            buttonDALF_Save.BackgroundColor = Color.Salmon;
+            buttonDALF_Save.BorderColor = Color.Salmon;
+            buttonDALF_Save.BorderRadius = 15;
+            buttonDALF_Save.BorderSize = 0;
+            buttonDALF_Save.FlatAppearance.BorderSize = 0;
+            buttonDALF_Save.FlatStyle = FlatStyle.Flat;
+            buttonDALF_Save.Font = new Font("Yu Gothic UI Semibold", 10F);
+            buttonDALF_Save.ForeColor = Color.White;
+            buttonDALF_Save.Location = new Point(12, 270);
+            buttonDALF_Save.Margin = new Padding(3, 4, 3, 4);
+            buttonDALF_Save.Name = "buttonDALF_Save";
+            buttonDALF_Save.Size = new Size(318, 43);
+            buttonDALF_Save.TabIndex = 36;
+            buttonDALF_Save.Text = "Save";
+            buttonDALF_Save.TextColor = Color.White;
+            buttonDALF_Save.UseVisualStyleBackColor = false;
+            // 
+            // rjTextBox2
+            // 
+            rjTextBox2.BackColor = SystemColors.Window;
+            rjTextBox2.BorderColor = Color.Salmon;
+            rjTextBox2.BorderFocusColor = Color.HotPink;
+            rjTextBox2.BorderRadius = 15;
+            rjTextBox2.BorderSize = 2;
+            rjTextBox2.Font = new Font("Microsoft Sans Serif", 9.5F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            rjTextBox2.ForeColor = Color.FromArgb(64, 64, 64);
+            rjTextBox2.Location = new Point(140, 187);
+            rjTextBox2.Margin = new Padding(5);
+            rjTextBox2.Multiline = false;
+            rjTextBox2.Name = "rjTextBox2";
+            rjTextBox2.Padding = new Padding(11, 9, 11, 9);
+            rjTextBox2.PasswordChar = false;
+            rjTextBox2.PlaceholderColor = Color.DarkGray;
+            rjTextBox2.PlaceholderText = "";
+            rjTextBox2.Size = new Size(192, 39);
+            rjTextBox2.TabIndex = 40;
+            rjTextBox2.Texts = "";
+            rjTextBox2.UnderlinedStyle = false;
+            // 
+            // rjTextBoxAdminPassword
+            // 
+            rjTextBoxAdminPassword.BackColor = SystemColors.Window;
+            rjTextBoxAdminPassword.BorderColor = Color.Salmon;
+            rjTextBoxAdminPassword.BorderFocusColor = Color.HotPink;
+            rjTextBoxAdminPassword.BorderRadius = 15;
+            rjTextBoxAdminPassword.BorderSize = 2;
+            rjTextBoxAdminPassword.Font = new Font("Microsoft Sans Serif", 9.5F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            rjTextBoxAdminPassword.ForeColor = Color.FromArgb(64, 64, 64);
+            rjTextBoxAdminPassword.Location = new Point(138, 137);
+            rjTextBoxAdminPassword.Margin = new Padding(5);
+            rjTextBoxAdminPassword.Multiline = false;
+            rjTextBoxAdminPassword.Name = "rjTextBoxAdminPassword";
+            rjTextBoxAdminPassword.Padding = new Padding(11, 9, 11, 9);
+            rjTextBoxAdminPassword.PasswordChar = false;
+            rjTextBoxAdminPassword.PlaceholderColor = Color.DarkGray;
+            rjTextBoxAdminPassword.PlaceholderText = "";
+            rjTextBoxAdminPassword.Size = new Size(192, 39);
+            rjTextBoxAdminPassword.TabIndex = 39;
+            rjTextBoxAdminPassword.Texts = "";
+            rjTextBoxAdminPassword.UnderlinedStyle = false;
+            // 
+            // rjTextBox1
+            // 
+            rjTextBox1.BackColor = SystemColors.Window;
+            rjTextBox1.BorderColor = Color.Salmon;
+            rjTextBox1.BorderFocusColor = Color.HotPink;
+            rjTextBox1.BorderRadius = 15;
+            rjTextBox1.BorderSize = 2;
+            rjTextBox1.Font = new Font("Microsoft Sans Serif", 9.5F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            rjTextBox1.ForeColor = Color.FromArgb(64, 64, 64);
+            rjTextBox1.Location = new Point(140, 88);
+            rjTextBox1.Margin = new Padding(5);
+            rjTextBox1.Multiline = false;
+            rjTextBox1.Name = "rjTextBox1";
+            rjTextBox1.Padding = new Padding(11, 9, 11, 9);
+            rjTextBox1.PasswordChar = false;
+            rjTextBox1.PlaceholderColor = Color.DarkGray;
+            rjTextBox1.PlaceholderText = "";
+            rjTextBox1.Size = new Size(192, 39);
+            rjTextBox1.TabIndex = 38;
+            rjTextBox1.Texts = "";
+            rjTextBox1.UnderlinedStyle = false;
+            // 
+            // rjTextBoxAdminIdentificationNo
+            // 
+            rjTextBoxAdminIdentificationNo.BackColor = SystemColors.Window;
+            rjTextBoxAdminIdentificationNo.BorderColor = Color.Salmon;
+            rjTextBoxAdminIdentificationNo.BorderFocusColor = Color.HotPink;
+            rjTextBoxAdminIdentificationNo.BorderRadius = 15;
+            rjTextBoxAdminIdentificationNo.BorderSize = 2;
+            rjTextBoxAdminIdentificationNo.Font = new Font("Microsoft Sans Serif", 9.5F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            rjTextBoxAdminIdentificationNo.ForeColor = Color.FromArgb(64, 64, 64);
+            rjTextBoxAdminIdentificationNo.Location = new Point(140, 39);
+            rjTextBoxAdminIdentificationNo.Margin = new Padding(5);
+            rjTextBoxAdminIdentificationNo.Multiline = false;
+            rjTextBoxAdminIdentificationNo.Name = "rjTextBoxAdminIdentificationNo";
+            rjTextBoxAdminIdentificationNo.Padding = new Padding(11, 9, 11, 9);
+            rjTextBoxAdminIdentificationNo.PasswordChar = false;
+            rjTextBoxAdminIdentificationNo.PlaceholderColor = Color.DarkGray;
+            rjTextBoxAdminIdentificationNo.PlaceholderText = "";
+            rjTextBoxAdminIdentificationNo.Size = new Size(192, 39);
+            rjTextBoxAdminIdentificationNo.TabIndex = 37;
+            rjTextBoxAdminIdentificationNo.Texts = "";
+            rjTextBoxAdminIdentificationNo.UnderlinedStyle = false;
+            // 
+            // labelDALF_Presc
+            // 
+            labelDALF_Presc.AutoSize = true;
+            labelDALF_Presc.Font = new Font("Yu Gothic UI Semibold", 11F);
+            labelDALF_Presc.Location = new Point(5, 201);
+            labelDALF_Presc.Name = "labelDALF_Presc";
+            labelDALF_Presc.Size = new Size(122, 25);
+            labelDALF_Presc.TabIndex = 44;
+            labelDALF_Presc.Text = "Prescriptions";
+            // 
+            // labelDALF_Diag
+            // 
+            labelDALF_Diag.AutoSize = true;
+            labelDALF_Diag.Font = new Font("Yu Gothic UI Semibold", 11F);
+            labelDALF_Diag.Location = new Point(5, 151);
+            labelDALF_Diag.Name = "labelDALF_Diag";
+            labelDALF_Diag.Size = new Size(109, 25);
+            labelDALF_Diag.TabIndex = 43;
+            labelDALF_Diag.Text = "Diagnosing";
+            // 
+            // labelDALF_PatiId
+            // 
+            labelDALF_PatiId.AutoSize = true;
+            labelDALF_PatiId.Font = new Font("Yu Gothic UI Semibold", 11F);
+            labelDALF_PatiId.Location = new Point(10, 53);
+            labelDALF_PatiId.Name = "labelDALF_PatiId";
+            labelDALF_PatiId.Size = new Size(98, 25);
+            labelDALF_PatiId.TabIndex = 42;
+            labelDALF_PatiId.Text = "Patient ID";
+            // 
+            // labelDALF_Patinam
+            // 
+            labelDALF_Patinam.AutoSize = true;
+            labelDALF_Patinam.Font = new Font("Yu Gothic UI Semibold", 11F);
+            labelDALF_Patinam.Location = new Point(5, 102);
+            labelDALF_Patinam.Name = "labelDALF_Patinam";
+            labelDALF_Patinam.Size = new Size(130, 25);
+            labelDALF_Patinam.TabIndex = 41;
+            labelDALF_Patinam.Text = "Patient Name";
+            // 
+            // DALF_RandevuGörüntüle
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(800, 450);
+            Controls.Add(labelDALF_Presc);
+            Controls.Add(labelDALF_Diag);
+            Controls.Add(labelDALF_PatiId);
+            Controls.Add(labelDALF_Patinam);
+            Controls.Add(rjTextBox2);
+            Controls.Add(rjTextBoxAdminPassword);
+            Controls.Add(rjTextBox1);
+            Controls.Add(rjTextBoxAdminIdentificationNo);
+            Controls.Add(buttonDALF_Save);
+            Controls.Add(buttonDALF_Del);
+            Controls.Add(buttonDALF_Upd);
+            Controls.Add(dataGridView1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "DALF_RandevuGörüntüle";
+            Text = "DALF_RandevuGörüntüle";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private DataGridView dataGridView1;
+        private CustomTools.RJButton.RJButton buttonDALF_Upd;
+        private CustomTools.RJButton.RJButton buttonDALF_Del;
+        private CustomTools.RJButton.RJButton buttonDALF_Save;
+        private CustomTools.RJTextBox.RJTextBox rjTextBox2;
+        private CustomTools.RJTextBox.RJTextBox rjTextBoxAdminPassword;
+        private CustomTools.RJTextBox.RJTextBox rjTextBox1;
+        private CustomTools.RJTextBox.RJTextBox rjTextBoxAdminIdentificationNo;
+        private Label labelDALF_Presc;
+        private Label labelDALF_Diag;
+        private Label labelDALF_PatiId;
+        private Label labelDALF_Patinam;
+    }
+}
